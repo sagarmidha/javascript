d510a84ad28b53a361edd353acb9f9f03e5b0791
sagarmidha <157627415+sagarmidha@users.noreply.github.com>
2024-07-21 10:54:37 +0530

    Datatypes in javascript

---
+++ 02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all jS code as newer version
+
+let name = "Akash" //string 
+let age = 18  // number
+let isdone = true // boolean
+
+
+// number , bigint,string,boolean-true false,null empty value not assigned , symbol -- use to find uniquness 
+
+console.log(typeof "sagar");      // string
+console.log(typeof true);  // boolean
+console.log(typeof 18); // number
+console.log(typeof null);  //object
+console.log(typeof undefined);   //undefined
