a1a04aa0519f717e81b19b5d427e74fb2ea20958
sagarmidha <157627415+sagarmidha@users.noreply.github.com>
2024-07-27 12:28:23 +0530

    js operations

---
+++ 04_operatins.js
@@ -0,0 +1,36 @@
+// for value  to be negative
+let value = 3
+let neg = -value 
+console.log(neg);
+// different operations 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**5);
+console.log(2/5);
+console.log(2%5);
+// string concatination
+let str="hello"
+let srt2 = "sagar"
+let str3= str + str2 
+console.log(str3); // hellosagar
+// different output with string and number 
+console.log("1"+2); // 12
+console.log("1"+"2"); // 12
+console.log(1+"2"); // 12
+console.log(1+2+"2"); // 32
+console.log("1"+2+2); // 122
+
+
+// this should not be done in coding as 
+let n1 = n2= n3 = 2+2
+console.log(+true);
+console.log(+"");
+
+
+//pre and post incremet 
+let gc = 122
+gc++;
+console.log(gc);
+++gc;
+console.log(gc);
